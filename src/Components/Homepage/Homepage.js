e3c1e33fe02db9b53d1b6eee359ffd5a5eb8756d
Franziska Kissling <frankissling@yahoo.co.uk>
2023-08-19 20:06:15 +0100

    text appears typed

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -9,6 +9,8 @@ export default function Homepage() {
 
   const [cursorVisible, setCursorVisible] = useState(true);
   const [introVisible, setIntroVisible] = useState(false);
+  const introText = "Hello.\nMy name is Franziska Kissling.\nI love to code.";
+  const [typedText, setTypedText] = useState('');
 
   useEffect(() => {
     // Animation for cursor effect
@@ -21,6 +23,7 @@ export default function Homepage() {
       setCursorVisible(false);
       setIntroVisible(true);
       clearInterval(cursorAnimationInterval); // Clear the cursor animation interval
+      startTyping();
     }, 3500);
 
     return () => {
@@ -29,6 +32,18 @@ export default function Homepage() {
     };
   }, []);
 
+  const startTyping = () => {
+    let currentIndex = 0;
+    const typingInterval = setInterval(() => {
+      if (currentIndex <= introText.length) {
+        setTypedText(introText.slice(0, currentIndex));
+        currentIndex++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 70); // Adjust typing speed here
+  };
+
   // Cursor display style
   const cursorStyle = {
     display: cursorVisible ? 'inline-block' : 'none'
@@ -36,18 +51,21 @@ export default function Homepage() {
 
   // Intro display style
   const introStyle = {
-    display: introVisible ? 'inline-block' : 'none'
+    display: introVisible ? 'block' : 'none'
   };
 
   return (
     <div className="h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${island})` }}>
       <p id="intro" className="tracking-widest font-handjet font-bold absolute top-14 text-4xl text-center leading-[3.5rem]">
         <span id="cursor" style={cursorStyle} className="before:w-3 before:h-6 before:bg-stone-600 before:inline-block animate-cursorAnimation"></span>
-        {introVisible && (
-          <span>
-            Hello.<br />My name is Franziska Kissling.<br />I love to code.
-          </span>
-        )}
+        <span style={introStyle} className="intro-text">
+          {typedText.split('\n').map((line, index) => (
+            <React.Fragment key={index}>
+              {line}
+              <br />
+            </React.Fragment>
+          ))}
+        </span>
       </p>
     </div>
   );
