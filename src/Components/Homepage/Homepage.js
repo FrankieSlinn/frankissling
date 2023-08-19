4113c1c7d4ef0db5250b161d1162bfb054f18f94
Franziska Kissling <frankissling@yahoo.co.uk>
2023-08-19 11:40:05 +0100

    added paragraph

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -6,8 +6,11 @@ const island = require('../../images/island.jpg');
 export default function Homepage() {
   return (
     <div className="h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${island})` }}>
-      {/* <p>Hello. My name is Franziska Kissling and I love to code.</p> */}
-    </div>
+
+  <p className="absolute top-20 font-handjet">Hello. My name is Franziska Kissling and I love to code.</p>
+</div>
+
+   
   )
 }
 
