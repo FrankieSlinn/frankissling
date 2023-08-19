c87495e5bbf705121b08215613fb6e028c33b346
Franziska Kissling <frankissling@yahoo.co.uk>
2023-08-19 17:14:47 +0100

    text higher

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -6,7 +6,7 @@ const island = require('../../images/island.jpg');
 export default function Homepage() {
   return (
     <div className="h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${island})` }}>
-<p className="tracking-widest font-handjet font-bold absolute top-16  text-4xl text-center leading-[3.5rem]" >
+<p className="tracking-widest font-handjet font-bold absolute top-14  text-4xl text-center leading-[3.5rem]" >
   Hello.<br />My name is Franziska Kissling.<br /> I love to code.
 </p>
 </div>
