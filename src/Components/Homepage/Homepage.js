bfa4d0e8714056e0edd487fdfa606aba692220ab
Franziska Kissling <frankissling@yahoo.co.uk>
2023-08-19 21:08:07 +0100

    cursors flashing right

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -7,27 +7,29 @@ const island = require('../../images/island.jpg');
 
 export default function Homepage() {
 
-  const [cursorVisible, setCursorVisible] = useState(true);
+  const [cursor1Visible, setCursor1Visible] = useState(true);
+  const [cursor2Visible, setCursor2Visible] = useState(false);
   const [introVisible, setIntroVisible] = useState(false);
-  const introText = "Hello.\nMy name is Franziska Kissling.\nI love to code.";
+  const introText = "Hello. My name is Franziska Kissling and I love to code.";
   const [typedText, setTypedText] = useState('');
 
   useEffect(() => {
-    // Animation for cursor effect
-    const cursorAnimationInterval = setInterval(() => {
-      setCursorVisible(prevVisible => !prevVisible);
+    // Animation for the first cursor effect
+    const cursor1AnimationInterval = setInterval(() => {
+      setCursor1Visible(prevVisible => !prevVisible);
     }, 500);
 
     // Show the intro after 4 seconds
     const showIntroTimer = setTimeout(() => {
-      setCursorVisible(false);
+      setCursor1Visible(false);
+      setCursor2Visible(true);
       setIntroVisible(true);
-      clearInterval(cursorAnimationInterval); // Clear the cursor animation interval
+      clearInterval(cursor1AnimationInterval); // Clear the first cursor animation interval
       startTyping();
-    }, 3500);
+    }, 4000);
 
     return () => {
-      clearInterval(cursorAnimationInterval);
+      clearInterval(cursor1AnimationInterval);
       clearTimeout(showIntroTimer);
     };
   }, []);
@@ -40,32 +42,43 @@ export default function Homepage() {
         currentIndex++;
       } else {
         clearInterval(typingInterval);
+        // Start the continuously flashing cursor at the end
+        startFlashingCursor();
       }
     }, 70); // Adjust typing speed here
   };
 
+  const startFlashingCursor = () => {
+    setInterval(() => {
+      setCursor2Visible(prevVisible => !prevVisible);
+    }, 500);
+  };
+
   // Cursor display style
-  const cursorStyle = {
-    display: cursorVisible ? 'inline-block' : 'none'
+  const cursor1Style = {
+    display: cursor1Visible ? 'inline-block' : 'none'
+  };
+  const cursor2Style = {
+    display: cursor2Visible ? 'inline-block' : 'none'
   };
 
   // Intro display style
   const introStyle = {
-    display: introVisible ? 'block' : 'none'
+    display: introVisible ? 'inline-block' : 'none'
   };
 
   return (
     <div className="h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${island})` }}>
       <p id="intro" className="tracking-widest font-handjet font-bold absolute top-14 text-4xl text-center leading-[3.5rem]">
-        <span id="cursor" style={cursorStyle} className="before:w-3 before:h-6 before:bg-stone-600 before:inline-block animate-cursorAnimation"></span>
+        <span id="cursor" style={cursor1Style} className="before:w-3 before:h-6 before:bg-stone-600 before:inline-block animate-cursorAnimation"></span>
         <span style={introStyle} className="intro-text">
           {typedText.split('\n').map((line, index) => (
             <React.Fragment key={index}>
               {line}
-              <br />
             </React.Fragment>
           ))}
         </span>
+        <span id="cursor2" style={cursor2Style} className="before:w-3 before:h-6 before:bg-stone-600 before:inline-block animate-cursorAnimation"></span>
       </p>
     </div>
   );
