1f10f12487ea8864fac8437bd82eaad33907ae40
Franziska Kissling <frankissling@yahoo.co.uk>
2023-08-19 19:38:08 +0100

    animation disappears text appears

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -7,36 +7,50 @@ const island = require('../../images/island.jpg');
 
 export default function Homepage() {
 
-  //cursor animation
   const [cursorVisible, setCursorVisible] = useState(true);
+  const [introVisible, setIntroVisible] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    // Animation for cursor effect
+    const cursorAnimationInterval = setInterval(() => {
       setCursorVisible(prevVisible => !prevVisible);
-    }, 900);
-  
-    const hideTimer = setTimeout(() => {
+    }, 500);
+
+    // Show the intro after 4 seconds
+    const showIntroTimer = setTimeout(() => {
       setCursorVisible(false);
-      clearInterval(interval); // Clear the animation interval
-    }, 4500);
-  
+      setIntroVisible(true);
+      clearInterval(cursorAnimationInterval); // Clear the cursor animation interval
+    }, 3500);
+
     return () => {
-      clearInterval(interval);
-      clearTimeout(hideTimer);
+      clearInterval(cursorAnimationInterval);
+      clearTimeout(showIntroTimer);
     };
   }, []);
+
+  // Cursor display style
   const cursorStyle = {
     display: cursorVisible ? 'inline-block' : 'none'
   };
+
+  // Intro display style
+  const introStyle = {
+    display: introVisible ? 'inline-block' : 'none'
+  };
+
   return (
     <div className="h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${island})` }}>
-<p className="tracking-widest font-handjet font-bold absolute top-14  text-4xl text-center leading-[3.5rem]" ><span id="cursor" style={cursorStyle} class="before:w-3 before:h-6 before:bg-stone-600 before:inline-block animate-cursorAnimation "></span>Hello.
-  <br />My name is Franziska Kissling.<br /> I love to code.
-</p>
-</div>
-
-   
-  )
+      <p id="intro" className="tracking-widest font-handjet font-bold absolute top-14 text-4xl text-center leading-[3.5rem]">
+        <span id="cursor" style={cursorStyle} className="before:w-3 before:h-6 before:bg-stone-600 before:inline-block animate-cursorAnimation"></span>
+        {introVisible && (
+          <span>
+            Hello.<br />My name is Franziska Kissling.<br />I love to code.
+          </span>
+        )}
+      </p>
+    </div>
+  );
 }
 
 
