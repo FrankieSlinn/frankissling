66a6a5ca26acde7d7093b9ac6ec58be425baeafd
Franziska Kissling <frankissling@yahoo.co.uk>
2023-08-20 16:58:05 +0100

    cursor is rectangle

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,30 +1,22 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Typewriter } from 'react-simple-typewriter';
+
 
 const island = require('../../images/island.jpg');
 
-
-
 export default function Homepage() {
-
 
 
   return (
     <div className="h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${island})` }}>
       <p id="intro" className="tracking-widest font-handjet font-bold absolute top-36 text-4xl text-center leading-[3.5rem]">
-      <Typewriter   words={["Hello. I'm Franziska and I love to code."]}
-            cursor
-            cursorStyle='|'
-            typeSpeed={150} />
+        <Typewriter
+          words={["Hello. I'm Franziska. I love coding."]}
+          cursor
+          cursorStyle={'▮'}
+          typeSpeed={150}
+        />
       </p>
     </div>
   );
 }
-
-
-
-
-
-
-
