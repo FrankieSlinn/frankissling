d76b56163a01e02a8a065d471cfeae6550787117
Franziska Kissling <frankissling@yahoo.co.uk>
2023-08-20 23:04:12 +0100

    cow falls from top to bottom

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import cow from '../../images/Cow.png';
 
 export default function Footer() {
+  const [showCow, setShowCow] = useState(false);
+
+  useEffect(() => {
+    // Show the cow after a delay
+    setTimeout(() => {
+      setShowCow(true);
+    }, 7000); // Adjust the delay as needed
+  }, []);
+
   return (
-    <div className="text-center flex justify-evenly items-center -mt-32 tracking-widest font-bold text-stone-900">
-    <div className="bg-opacity-0">
-      <img className="h-20 w-20" src={cow} alt="About Me" />
-      <span >About Me</span>
-    </div>
+    <div className="fixed left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen pointer-events-none">
+      <div
+        className={`transition-transform duration-15000 ${
+          showCow ? 'absolute bottom-0 translate-y-0' : 'absolute top-0 -translate-y-full'
+        }`}
+      >
+        <img className="h-20 w-20" src={cow} alt="Cow" />
+        <span>About Me</span>
+      </div>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
